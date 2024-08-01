2e3552b5e628d1925e676c722c6b6028138e9d2c
Marcin Kuder <marcin.kuder@gmail.com>
2024-08-01 18:44:52 +0200

    atlas builder in linux scripts

--- build_release.sh
+++ build_release.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+# Uncomment these lines if you want to build an atlas from any aseprite files in a `textures` subfolder.
+# odin build atlas_builder -use-separate-modules -out:atlas_builder.bin -strict-style -vet-using-stmt -vet-using-param -vet-semicolon -debug
+# if [ ! $? -eq 0 ]; then
+#     exit 1
+# fi
+# ./atlas_builder.bin
+# if [ ! $? -eq 0 ]; then
+#     exit 1
+# fi
+# chmod +rw atlas.odin
+
 odin build main_release -out:game_release.bin -no-bounds-check -o:speed -strict-style -vet-unused -vet-using-stmt -vet-using-param -vet-style -vet-semicolon
