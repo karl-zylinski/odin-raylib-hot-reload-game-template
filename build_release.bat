453e86b4abdf70442ce2b0969c62b335f8bd8bea
Karl Zylinski <karl@zylinski.se>
2024-08-16 14:21:08 +0200

    Simplify windows release build script

--- build_release.bat
+++ build_release.bat
@@ -1,9 +1,10 @@
 @echo off
 
+set BUILD_PARAMS=-strict-style -vet-unused -vet-using-stmt -vet-using-param -vet-style -vet-semicolon
+
 rem Uncomment these lines if you want to build an atlas from any aseprite files in a `textures` subfolder.
-rem odin build atlas_builder -use-separate-modules -out:atlas_builder.exe -strict-style -vet-using-stmt -vet-using-param -vet-style -vet-semicolon -debug
-rem IF %ERRORLEVEL% NEQ 0 exit /b 1
-rem atlas_builder.exe
+rem Run atlas builder, which outputs atlas.odin and atlas.png
+rem odin run atlas_builder %BUILD_PARAMS%
 rem IF %ERRORLEVEL% NEQ 0 exit /b 1
 
-odin build main_release -define:RAYLIB_SHARED=false -out:game_release.exe -no-bounds-check -o:speed -strict-style -vet-unused -vet-using-stmt -vet-using-param -vet-style -vet-semicolon -subsystem:windows
+odin build main_release -define:RAYLIB_SHARED=false -out:game_release.exe -no-bounds-check -o:speed %BUILD_PARAMS% -subsystem:windows
