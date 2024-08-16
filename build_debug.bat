299edecb4ba88054eefa1850b7aa834419fc5640
Karl Zylinski <karl@zylinski.se>
2024-08-16 14:22:05 +0200

    Simplified windows debug build script

--- build_debug.bat
+++ build_debug.bat
@@ -1,2 +1,8 @@
 @echo off
-odin build main_release -define:RAYLIB_SHARED=false -out:game_debug.exe -no-bounds-check -subsystem:windows -debug
+
+rem Uncomment these lines if you want to build an atlas from any aseprite files in a `textures` subfolder.
+rem Run atlas builder, which outputs atlas.odin and atlas.png
+rem odin run atlas_builder -debug
+rem IF %ERRORLEVEL% NEQ 0 exit /b 1
+
+odin build main_release -define:RAYLIB_SHARED=false -out:game_debug.exe -subsystem:windows -debug
