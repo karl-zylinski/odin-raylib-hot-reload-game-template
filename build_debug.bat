0a412eaf2f36beaab4077455a8d7dbba09521319
Karl Zylinski <karl@zylinski.se>
2024-09-18 00:27:50 +0200

    Remove atlas builder stuff from build script

--- build_debug.bat
+++ build_debug.bat
@@ -1,10 +1,5 @@
 @echo off
 
-rem Run atlas builder, which outputs game/atlas.odin and atlas.png
-rem Note: You'll have to modify atlas_builder.odin to output atlas.odin to the game subfolder.
-rem odin run atlas_builder -debug
-rem IF %ERRORLEVEL% NEQ 0 exit /b 1
-
 set BUILD_PARAMS=-strict-style -vet-using-stmt -vet-using-param -vet-style -vet-semicolon -debug
 
 odin build main_release -define:RAYLIB_SHARED=false -out:game_debug.exe -subsystem:windows %BUILD_PARAMS%
--- build_hot_reload.sh
+++ build_hot_reload.sh
@@ -2,14 +2,6 @@
 
 VET="-strict-style -vet-unused -vet-using-stmt -vet-using-param -vet-style -vet-semicolon"
 
-# Run atlas builder, which outputs game/atlas.odin and atlas.png
-# Note: You'll have to modify atlas_builder.odin to output atlas.odin to the game subfolder.
-# odin run atlas_builder -out:atlas_builder.bin -debug $VET
-# if [ ! $? -eq 0 ]; then
-#     exit 1
-# fi
-# chmod +rw game/atlas.odin
-
 # NOTE: this is a recent addition to the Odin compiler, if you don't have this command
 # you can change this to the path to the Odin folder that contains vendor, eg: "~/Odin".
 ROOT=$(odin root)
