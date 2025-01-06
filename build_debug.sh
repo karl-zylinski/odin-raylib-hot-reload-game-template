9738feb974720689519c2826b58c61cbe5c7ae2a
Karl Zylinski <karl@zylinski.se>
2025-01-06 18:04:05 +0100

    Build sucesss messages

--- build_debug.sh
+++ build_debug.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# This creates a build that is similar to a release build, but it is debuggable.
+# There is no hot reloading and no separate game library.
+
 OUT_DIR="build/debug"
 mkdir -p "$OUT_DIR"
 odin build source/main_release -out:$OUT_DIR/game_debug.bin -strict-style -vet -debug
 cp -R assets $OUT_DIR
+echo "Debug build created in $OUT_DIR"
--- build_release.sh
+++ build_release.sh
@@ -4,3 +4,4 @@ OUT_DIR="build/release"
 mkdir -p "$OUT_DIR"
 odin build source/main_release -out:$OUT_DIR/game_release.bin -strict-style -vet -no-bounds-check -o:speed
 cp -R assets $OUT_DIR
+echo "Release build created in $OUT_DIR"
